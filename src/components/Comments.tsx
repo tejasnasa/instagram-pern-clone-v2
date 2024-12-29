9e224b3034e394f517be372b52c19f4930c07499
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-12-29 18:43:39 +0530

    Fix bugs

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { useState } from "react";
-import { redirect } from "react-router-dom";
 
 interface CommentsProps {
   comments: {
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-// src/components/Header.tsx
 import React from "react";
 import { useLocation } from "react-router-dom";
 
